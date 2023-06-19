b8e4109b862cfade65a0565541958aadb2342bb6
Efaideleon <eedeleon@uci.edu>
2023-06-19 15:53:41 -0700

    blocking

---
+++ TEST-NAME/Assets/Scripts/CottonSpawnManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CottonSpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject cottonPrefab;
+
+    [SerializeField]
+    GameObject leftWall;
+
+    [SerializeField]
+    GameObject rightWall;
+
+    [SerializeField]
+    GameObject topLeftWall;
+
+    [SerializeField]
+    GameObject topRightWall;
+
+    [SerializeField]
+    GameObject bottomWall;
+
+    private int xRange;
+    private int yRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCotton();
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void SpawnCotton() {
+        xRange = Mathf.Abs(Mathf.RoundToInt(rightWall.transform.position.x));
+        yRange = Mathf.Abs(Mathf.RoundToInt(topLeftWall.transform.position.y) - 1);
+
+        for (int i = 0; i < xRange; i++)
+        {
+            for (int j = 0; j < yRange; j++)
+            {
+                Instantiate(
+                    cottonPrefab,
+                    new Vector3((leftWall.transform.position.x - 1) - i, 1 + j, 0),
+                    cottonPrefab.transform.rotation
+                );
+            }
+        }
+    }
+}
