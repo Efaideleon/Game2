b15e7b7f64c68f1d3a3827b2fc786e158e4ad9ed
Efaideleon <eedeleon@uci.edu>
2023-06-19 22:25:36 -0700

    stuff

--- TEST-NAME/Assets/Scripts/PlayerMovement.cs
+++ TEST-NAME/Assets/Scripts/PlayerMovement.cs
@@ -6,30 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1f; // The speed at which the player moves
 
-    void Start()
-    {
-    } 
-    
+    void Start() { }
+
     void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal"); // Get the horizontal input axis
-        float y = Input.GetAxis("Vertical"); // Get the vertical input axis
+        float x = Input.GetAxisRaw("Horizontal"); // Get the horizontal input axis
+        float y = Input.GetAxisRaw("Vertical"); // Get the vertical input axis
         MoveOneUnit(x, y);
     }
 
-    void MoveOneUnit(float x, float y) {
-        Vector3 move = transform.right * -x + transform.up * y; // Calculate the movement vector
-
-        if (move.x < 0) {
-            move.x = Mathf.Floor(move.x);
-            move.y = Mathf.Floor(move.y);
-        }
-        else{
-            move.x = (float) Mathf.Ceil(move.x);
-            move.y = (float) Mathf.Ceil(move.y);
-        }
-
-        transform.position += move * speed * Time.fixedDeltaTime; // Move the player
+    void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.CompareTag("Cotton");
+        Destroy(collision.gameObject);
+        Debug.Log("Collision!");
     }
 
+    void MoveOneUnit(float x, float y)
+    {
+        if (x != 0 && y != 0)
+        {
+            x = Mathf.Round(x);
+            y = 0;
+        }
+        Vector3 move = new Vector3(-x, y, 0);
+        Vector3 direction = move * speed * Time.fixedDeltaTime;
+        transform.position += direction; // Move the player
+    }
 }
