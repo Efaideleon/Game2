306cec329227c9924ac6b9c6ccbc9a547f6fa326
Efaideleon <eedeleon@uci.edu>
2023-06-20 14:20:30 -0700

    Update PlayerMovement.cs

--- TEST-NAME/Assets/Scripts/PlayerMovement.cs
+++ TEST-NAME/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1f; // The speed at which the player moves
+    public float x;
+    public float y;
+    public enum CurrentInput
+    {
+        Horizontal,
+        Vertical,
+        None
+    }
+
+    public CurrentInput currInput;
 
     void Start() { }
 
     void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal"); // Get the horizontal input axis
-        float y = Input.GetAxisRaw("Vertical"); // Get the vertical input axis
+        x = Input.GetAxisRaw("Horizontal"); // Get the horizontal input axis
+        y = Input.GetAxisRaw("Vertical"); // Get the vertical input axis
         MoveOneUnit(x, y);
     }
 
@@ -22,17 +32,59 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Collision!");
     }
 
-    void MoveOneUnit(float x, float y)
+    void MoveOneUnit(float x_, float y_)
     {
-        if (x != 0)
+        if (x_ != 0)
         {
-            y = 0;
+            if (currInput == CurrentInput.Horizontal && y_ != 0) {
+                currInput = CurrentInput.Vertical;
+            }
+            else if(currInput != CurrentInput.Vertical) 
+            {
+                currInput = CurrentInput.Horizontal;
+            }
         }
-        if (y != 0)
+        else if (y_ != 0)
         {
-            x = 0;
+            if (currInput == CurrentInput.Vertical && x_ != 0){
+                currInput = CurrentInput.Horizontal;
+            }
+            else if (currInput != CurrentInput.Horizontal)
+            {
+                currInput = CurrentInput.Vertical;
+            }
         }
-        Vector3 move = new Vector3(-x, y, 0);
+
+        if (currInput == CurrentInput.Horizontal)
+        {
+            MoveHorizontal(x_);
+        }
+        else if (currInput == CurrentInput.Vertical)
+        {
+            MoveVertical(y_);
+        }
+
+        if (x_ == 0 && y_ ==0){
+            currInput = CurrentInput.None;
+        }
+    }
+
+    void MoveHorizontal(float x_)
+    {
+        int moveX = Mathf.RoundToInt(x_);
+        Vector3 move = new Vector3(-moveX, 0, 0);
+        Move(move);
+    }
+
+    void MoveVertical(float y_)
+    {
+        int moveY = Mathf.RoundToInt(y_);
+        Vector3 move = new Vector3(0, moveY, 0);
+        Move(move);
+    }
+
+    void Move(Vector3 move)
+    {
         Vector3 direction = move * speed * Time.fixedDeltaTime;
         transform.position += direction; // Move the player
     }
