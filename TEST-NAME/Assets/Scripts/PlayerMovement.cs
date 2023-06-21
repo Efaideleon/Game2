8c7bcb1f34202169f0adda1545d7cdd804e9f32a
Efaideleon <eedeleon@uci.edu>
2023-06-21 10:51:33 -0700

    idk

--- TEST-NAME/Assets/Scripts/PlayerMovement.cs
+++ TEST-NAME/Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 2f; // The speed at which the player moves
+    public float speed = 1f; // The speed at which the player moves
     public float x;
     public float y;
     enum MoveDirection
