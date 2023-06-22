2bbbe2f7baba60683d5108608c04c23ab1f295cc
Efaideleon <eedeleon@uci.edu>
2023-06-21 18:15:24 -0700

    setting up prefabs plus animator controller

--- TEST-NAME/Assets/Scripts/PlayerController.cs
+++ TEST-NAME/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Animator playerAnimator;
     [SerializeField] AudioClip cutSound;
     AudioSource playerAudio;
     void Start()
     {
         playerAudio = GetComponent<AudioSource>();
+        playerAnimator = GetComponent<Animator>();
     }
     
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Cotton"))
         {
+            playerAnimator.SetBool("Is_cutting_b", true);
             playerAudio.PlayOneShot(cutSound, 1.0f);
         }
     }
